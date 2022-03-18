55fa634cca516209a1cbf6f11815401c99c6a44a
suchmememanyskill <suchmememanyskill@users.noreply.github.com>
2022-03-18 13:18:15 +0100

    wagu

--- SimpleSteamShortcutAdder/Program.cs
+++ SimpleSteamShortcutAdder/Program.cs
@@ -1,44 +1,55 @@
 ﻿using System;
 using System.IO;
+using System.Threading;
 
 namespace SimpleSteamShortcutAdder
 {
     class Program
     {
+        static void Fatal(string message)
+        {
+            Console.WriteLine($"[FATAL] {message}");
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+
         static void Main(string[] args)
         {
             if (args.Length != 1)
             {
-                Console.WriteLine("[FATAL] Not enough or too many args!");
+                Fatal("Not enough or too many args!");
                 return;
             }
 
             SteamManager manager = new SteamManager();
             if (!manager.InitialisePaths())
             {
-                Console.WriteLine("[FATAL] Could not initialise paths. Does a shortcuts.vdf file exist? Try to add a non-steam game to steam");
+                Fatal("Could not initialise paths. Does a shortcuts.vdf file exist? Try to add a non-steam game to steam");
                 return;
             }
 
+            Console.WriteLine($"Accessing '{manager.VdfPath}'");
+
             if (!manager.Read())
             {
-                Console.WriteLine("[FATAL] Failed to read shortcuts.vdf");
+                Fatal("Failed to read shortcuts.vdf");
                 return;
             }
 
             if (!manager.AddExe(args[0], Path.GetFileName(args[0])))
             {
-                Console.WriteLine("[FATAL] Failed to add executable");
+                Fatal("Failed to add executable");
                 return;
             }
 
             if (!manager.Write())
             {
-                Console.WriteLine("[FATAL] Failed to write to the shortcuts.vdf file");
+                Fatal("Failed to write to the shortcuts.vdf file");
                 return;
             }
         
-            Console.WriteLine("Finished!");
+            Console.WriteLine("Finished! Exiting in 5 seconds. Restart steam to view the new shortcut");
+            Thread.Sleep(5000);
         }
     }
 }
